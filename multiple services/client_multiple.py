98b3460ad8452d2f1449ac74fd703dcb4762424f
Joakim Kiugi <kiugijoakim@gmail.com>
2026-06-19 21:09:14 +0300

    Add client implementation for multiple Pyro4 services

--- multiple services/client_multiple.py
+++ multiple services/client_multiple.py
@@ -1,1 +1,15 @@
+import Pyro4
 
+try:
+    # Connect to all services
+    greeting = Pyro4.Proxy("PYRONAME:example.greeting")
+    calculator = Pyro4.Proxy("PYRONAME:example.calculator")
+    student = Pyro4.Proxy("PYRONAME:example.student")
+    
+    # Use services
+    print(greeting.greet())
+    print(f"15 + 10 = {calculator.add(15, 10)}")
+    print(student.getStudent())
+    
+except Pyro4.errors.CommunicationError:
+    print("Server not available!")
