e9d290f0e44df385136958434cf81c7e37dfb879
Joakim Kiugi <kiugijoakim@gmail.com>
2026-06-23 19:24:06 +0300

    Implement counter service with persistent storage

--- persistence/counter_server.py
+++ persistence/counter_server.py
@@ -1,1 +1,32 @@
+import Pyro4
+import os
 
+@Pyro4.expose
+class CounterService:
+    def __init__(self):
+        self.count = self.load_count()
+    
+    def load_count(self):
+        try:
+            with open('counter.txt', 'r') as f:
+                return int(f.read())
+        except:
+            return 0
+    
+    def save_count(self):
+        with open('counter.txt', 'w') as f:
+            f.write(str(self.count))
+    
+    def increment(self):
+        self.count += 1
+        self.save_count()
+        return self.count
+    
+    def getCount(self):
+        return self.count
+
+daemon = Pyro4.Daemon()
+ns = Pyro4.locateNS()
+ns.register("example.counter", daemon.register(CounterService))
+print("Counter server ready...")
+daemon.requestLoop()
